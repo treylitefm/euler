5b809a2eb55648fe3fffff542c11cddecfeb8668
TreyLiteFM <griffin.omar@gmail.com>
2015-04-21 21:33:32 -0400

    Traverse algorithm scales surprisingly well. Just need a way to count the traversals.

--- problem15.php
+++ problem15.php
@@ -8,9 +8,10 @@ $root = sqrt($n); //but too lazy atm. Will fix in future graph implementations.
 
 $graph = new Graph($n);
 
-$graph->printGraph();
+//$graph->printGraph();
 
 //$paths = $graph->countTraversals2($graph->vertices[0], $n-1);
+$paths = $graph->countTraversals();
 
 print "On a $root x $root grid, there are $paths unique paths from the top left corner to the bottom left corner.\n";
 var_dump(memory_get_usage());
@@ -62,6 +63,9 @@ class Graph
 		}
 	}
 
+	/**
+	 * maxes out memory when using grids larger than 5x5 (max mem 137 megs)
+	 */
 	public function countTraversals2($vertex, $destination) //, $moves = [], $count = 0)
 	{
 		static $moves = [];
@@ -79,42 +83,49 @@ class Graph
 
 		if ($vertex->name == $destination) { //retreat
 			$count++;
-			return $this->countTraversals(array_pop($moves), $destination);//, $moves, $count);
+			return $this->countTraversals2(array_pop($moves), $destination);//, $moves, $count);
 		} elseif ($vertex->hasUnvisitedEdges()) { //advance
 			array_push($moves, $vertex);
-			return $this->countTraversals($this->getUnvisitedVertex($vertex), $destination);//, $moves, $count);
+			return $this->countTraversals2($this->getUnvisitedVertex($vertex), $destination);//, $moves, $count);
 		} else { //retreat
 			$vertex->cleanEdges();
-			return $this->countTraversals(array_pop($moves), $destination);//, $moves, $count);
+			return $this->countTraversals2(array_pop($moves), $destination);//, $moves, $count);
 		}
 	}
 
 	/**
-	 * @todo: rewrite so that redundantly visited nodes automagically
-	 * return number of paths instead of traversing to the destination
+	 * barely uses a meg on 20x20 grid
 	 */
-	public function countTraversals($vertex, $destination) //, $moves = [], $count = 0)
+	public function countTraversals()
 	{
-		static $moves = [];
-		static $count = 0; 
+		//var_dump(memory_get_usage());
+		$visited = [];
 
-		var_dump(memory_get_usage());
-	//	print_r([$vertex, $destination, $moves, $count]);
-
-		if (empty($vertex)) {
-			return $count;
+		for ($i = 0; $i < count($this->vertices); $i++) {
+			if (empty($visited[$i])) {
+				$count = $this->traverseDepthFirst($i, $visited);
+			}
 		}
 
-		if ($vertex->name == $destination) { //retreat
-			$count++;
-			return $this->countTraversals($this->vertices[array_pop($moves)], $destination);//, $moves, $count);
-		} elseif ($vertex->hasUnvisitedEdges()) { //advance
-			array_push($moves, $vertex->name);
-			return $this->countTraversals($this->getUnvisitedVertex($vertex), $destination);//, $moves, $count);
-		} else { //retreat
-			$vertex->cleanEdges();
-			return $this->countTraversals($this->vertices[array_pop($moves)], $destination);//, $moves, $count);
-		}
+		var_dump($count);
+	}
+
+	private function traverseDepthFirst($i, &$visited)
+	{
+		//var_dump(memory_get_usage());
+		static $count = 0;
+
+		$visited[$i] = true;
+		for ($edge = $this->vertices[$i]->firstEdge; $edge !== null; $edge = $edge->next) {
+			if (empty($visited[$edge->destination])) {
+				print "{$this->vertices[$i]->name} --> {$this->vertices[$edge->destination]->name}\n";
+				$this->traverseDepthFirst($edge->destination, $visited);
+			} else {
+				$count += $this->vertices[$i]->getEdgeCount();
+			}
+		}	
+
+		return $count;
 	}
 
 	private function getUnvisitedVertex(Vertex $vertex)
@@ -148,6 +159,18 @@ class Vertex
 		$this->name = $name;
 	}
 
+	public function getEdgeCount()
+	{
+		$count = 0;
+		$current = $this->firstEdge;
+
+		while ($current !== null) {
+			$current = $current->next;
+			$count++;
+		}
+
+		return $count;
+	}
 
 	public function addEdge($edge)
 	{
