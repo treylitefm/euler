49f81a0bee7c0e94f88eda1229ec2ea6a425aa93
TreyLiteFM <griffin.omar@gmail.com>
2015-04-30 23:00:01 -0400

    Set error reporting so that tests wont error as easily.

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-//require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/lib/autoload.php';
 
 date_default_timezone_set('America/Vancouver');
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
